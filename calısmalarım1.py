0f7dd5a3736cbeaa5c9f45d8110bf34fedd3774f
Sevde Karabacak <sevdeekarabacak@gmail.com>
2023-02-16 20:16:43 +0300

    Update calısmalarım1.py

--- calısmalarım1.py
+++ calısmalarım1.py
@@ -1,6 +1,7 @@
 ############################################################################
 # Python Veri Giriş Komutları
 ############################################################################
+# Ekrana belirli bir mesaj yazdırmak için klavyeden alfasayısal veri girişini sağlayan komut "input" tur.
 
  age = int(input('What is you age?'))
  name = str(input("What is you name?"))
