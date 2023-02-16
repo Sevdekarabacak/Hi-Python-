bbeef820ca5eaea53688c5e7b8f2c7b1a17437fd
Sevde Karabacak <20020152@stu.omu.edu.tr>
2023-02-16 20:13:38 +0300

    Add files via upload

---
+++ calısmalarım1.py
@@ -0,0 +1,17 @@
+############################################################################
+# Python Veri Giriş Komutları
+############################################################################
+
+ age = int(input('What is you age?'))
+ name = str(input("What is you name?"))
+ height = float(input("what is you height?"))
+
+
+
+
+
+
+
+
+
+
