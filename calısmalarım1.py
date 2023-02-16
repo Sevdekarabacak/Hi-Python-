107002b269735b1926dcf485aab68f1da0060747
Sevde Karabacak <20020152@stu.omu.edu.tr>
2023-02-16 21:37:05 +0300

    Add files via upload

---
+++ calısmalarım1.py
@@ -0,0 +1,35 @@
+############################################################################
+# Python Veri Giriş ve Çıkış Komutları
+############################################################################
+
+ age = int(input('What is you age?'))
+ name = str(input("What is you name?"))
+ height = float(input("what is you height?"))
+
+ print('sevde')
+ print("sevde")
+ print("""sevde""")
+
+ print("sevde","karabacak",sep="-")
+ print("sev\'de")
+ print("Sevde Karabacak\nİstanbul\t21")
+ print("Sevde","Karabacak","İstanbul","21",sep="-")
+ print("Sevde","\tKarabacak","\tİstanbul","\t21",sep="   ")
+ print("Sevde","\tKarabacak","\nİstanbul","\t21",sep="   ")
+ print("Sevde","Karabacak","İstanbul","21",sep=",",end=".")
+
+ a="sevde"
+ b="karabacak"
+ c="21"
+
+ print("name: {},\nsurname: {},\nage: {}".format(a,b,c))
+ print("name: {0},surname: {1},age: {2}".format(a,b,c))
+
+
+
+
+
+
+
+
+
